209e942301924a058d4a7bdf34e4f21528ed4a46
dmarkovtsev <denis@inflectra.com>
2020-02-27 18:01:27 +0300

    CreatePriceList test case added

---
+++ TC_CreatePriceList/Objects.js
@@ -0,0 +1,444 @@
+var saved_script_objects={
+	"Name": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Name",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: New Price List - Microsoft Dynamics 365",
+		"xpath": "//input[@type='text' and @data-id='name.fieldControl-text-box-text']",
+		"title": "Price List: Price Level: New Price List - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Start_Date": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Start Date",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: New Price List - Microsoft Dynamics 365",
+		"xpath": "//input[@role='combobox' and @type='text' and @data-id='begindate.fieldControl-date-time-input']",
+		"title": "Price List: Price Level: New Price List - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"End_Date": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "End Date",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: New Price List - Microsoft Dynamics 365",
+		"xpath": "//input[@role='combobox' and @type='text' and @data-id='enddate.fieldControl-date-time-input']",
+		"title": "Price List: Price Level: New Price List - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Description": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Description",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: New Price List - Microsoft Dynamics 365",
+		"xpath": "//textarea[@data-id='description.fieldControl-text-box-text']",
+		"title": "Price List: Price Level: New Price List - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Save": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Cell",
+		"object_name": "Save",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: New Price List - Microsoft Dynamics 365",
+		"xpath": "//button[@type='button' and @data-id='pricelevel|NoRelationship|Form|Mscrm.Form.pricelevel.Save' and @data-lp-id='Form:pricelevel-pricelevel|NoRelationship|Form|Mscrm.Form.pricelevel.Save']/span/span[2]",
+		"title": "Price List: Price Level: New Price List - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Price_List_Items": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Generic",
+		"object_name": "Price List Items",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"xpath": "//li[@role='tab' and @data-id='tablist-Price_List_Items' and @data-lp-id='form-tab-Price_List_Items']",
+		"title": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"New_Price_List_Item": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Cell",
+		"object_name": "New Price List Item",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"xpath": "//button[@type='button' and @data-id='productpricelevel|NoRelationship|SubGridStandard|Mscrm.SubGrid.productpricelevel.AddNewStandard' and @data-lp-id='SubGridStandard:productpricelevel-productpricelevel|NoRelationship|SubGridStandard|Mscrm.SubGrid.productpricelevel.AddNewStandard']/span/span[2]",
+		"title": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Product__Lookup": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Product  Lookup",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List Item: Information: New Price List Item - Microsoft Dynamics 365",
+		"xpath": "//input[@type='text' and @data-id='productid.fieldControl-LookupResultsDropdown_productid_textInputBox_with_filter_new']",
+		"title": "Price List Item: Information: New Price List Item - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Unit__Lookup": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Unit  Lookup",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List Item: Information: New Price List Item - Microsoft Dynamics 365",
+		"xpath": "//input[@type='text' and @data-id='uomid.fieldControl-LookupResultsDropdown_uomid_textInputBox_with_filter_new']",
+		"title": "Price List Item: Information: New Price List Item - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Amount": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Amount",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List Item: Information: New Price List Item - Microsoft Dynamics 365",
+		"xpath": "//input[@type='text' and @data-id='amount.fieldControl-currency-text-input']",
+		"title": "Price List Item: Information: New Price List Item - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Save_&_Close": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Cell",
+		"object_name": "Save & Close",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List Item: Information: New Price List Item - Microsoft Dynamics 365",
+		"xpath": "//button[@type='button' and @data-id='productpricelevel|NoRelationship|Form|Mscrm.Form.productpricelevel.SaveAndClose' and @data-lp-id='Form:productpricelevel-productpricelevel|NoRelationship|Form|Mscrm.Form.productpricelevel.SaveAndClose']/span/span[2]",
+		"title": "Price List Item: Information: New Price List Item - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"PriceListGrid": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "DomDynamicsCrmUnifiedInterfaceGrid",
+		"object_flavor": "Grid",
+		"object_name": "Grid",
+		"ignore_object_name": true,
+		"object_library": "DomDynamicsCrm",
+		"window_name": "Price Lists Active Price Lists - Microsoft Dynamics 365",
+		"xpath": "//div[@role='presentation' and @data-id='entity_control_container' and @data-lp-id='MscrmControls.Grid.ReadOnlyGrid|entity_control|pricelevel|eec94b91-98c3-43ca-a2e6-4cb36ddf71fc|pricelevel' and @data-type='Grid' and contains(concat(' ', @class, ' '), ' Grid.ReadOnlyGrid ')]",
+		"title": "Price Lists Active Price Lists - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Delete": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Cell",
+		"object_name": "Delete",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"xpath": "//button[@type='button' and @data-id='pricelevel|NoRelationship|Form|Mscrm.Form.pricelevel.Delete' and @data-lp-id='Form:pricelevel-pricelevel|NoRelationship|Form|Mscrm.Form.pricelevel.Delete']/span/span[2]",
+		"title": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Delete1": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "Delete",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"xpath": "//button[@type='button' and @data-id='confirmButton' and @id='confirmButton']",
+		"title": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"PriceListItemsGrid": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "DomDynamicsCrmUnifiedInterfaceGrid",
+		"object_flavor": "Grid",
+		"object_name": "Grid",
+		"ignore_object_name": true,
+		"object_library": "DomDynamicsCrm",
+		"window_name": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"xpath": "//div[@role='presentation' and @data-id='pricelistitemsgrid_container' and @data-lp-id='MscrmControls.Grid.ReadOnlyGrid|pricelistitemsgrid|pricelevel|f89e7f79-8829-4433-b4cc-b1c1e2829a95|productpricelevel|price_level_product_price_levels' and @data-type='Grid' and contains(concat(' ', @class, ' '), ' Grid.ReadOnlyGrid ')]",
+		"title": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"Delete_Price_List_Item": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Cell",
+		"object_name": "Delete Price List Item",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"xpath": "//button[@type='button' and @data-id='productpricelevel|NoRelationship|SubGridStandard|Mscrm.SubGrid.productpricelevel.Delete' and @data-lp-id='SubGridStandard:productpricelevel-productpricelevel|NoRelationship|SubGridStandard|Mscrm.SubGrid.productpricelevel.Delete']/span/span[2]",
+		"title": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"url": ""
+	},
+	"General": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "Chrome Legacy Window",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Generic",
+		"object_name": "General",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"xpath": "//li[@role='tab' and @data-id='tablist-General' and @data-lp-id='form-tab-General']",
+		"title": "Price List: Price Level: TestPriceList - Microsoft Dynamics 365",
+		"url": ""
+	}
+};
